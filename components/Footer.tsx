bbe021adfb6a07f692758e70950dbbba392c2454
bucky0112 <bucky0112@gmail.com>
2022-08-03 00:34:02 +0800

    add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react"
+
+const Footer: FunctionComponent = () => (
+  <footer className='w-full bg-slate-700 py-5'>
+    <p className="text-center text-white">Powered by マンガランク - Manga Rank</p>
+  </footer>
+)
+
+export { Footer }
