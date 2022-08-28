2b296d31a697ce644d1b059fc4e5545784e342c4
bucky0112 <bucky0112@gmail.com>
2022-08-28 21:36:15 +0800

    register and login page style ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ const Home: FunctionComponent = () => {
   return (
     <>
       <Navbar />
-      <main className='flex flex-col justify-center gap-10 p-10 bg-index bg-fixed bg-[length:200px_100px] bg-no-repeat'>
+      <main className='flex flex-col justify-center gap-10 p-10 bg-index bg-fixed bg-[length:300px] bg-no-repeat'>
         <CardGroup showBooks={showBooks} type='最新發行' />
         <CardGroup showBooks={showBooks} type='評價超過8分的作品' />
         <CardGroup showBooks={showBooks} type='各書店平台週冠軍' />
