7d544b6d001bf9e41453a5dbf6e9969cf45611d7
bucky0112 <bucky0112@gmail.com>
2022-09-20 01:09:49 +0800

    edit API

--- components/index/CardGroup.tsx
+++ components/index/CardGroup.tsx
@@ -1,29 +1,47 @@
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { BookCard } from '../shared'
+import { manga } from '../../lib/api/manga'
 
 interface book {
-  id: number
-  title: string
-  tag1: string
-  tag2: string
-  cover: string
+  uuid: string
+  title_cn: string
+  // tag1: string
+  // tag2: string
+  image: string
 }
 
 interface props {
-  showBooks: book[]
+  // showBooks: book[]
   type: string
 }
 
-const CardGroup = ({ showBooks, type }: props) => {
+const CardGroup = ({
+  // showBooks,
+  type }: props) => {
+  const [showBooks, setShowBooks] = useState<book[]>([])
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const { data } = await manga.getBooks(10)
+        const res = data?.data?.slice(0, 7)
+        setShowBooks(res)
+      } catch (err) {
+        console.error(err)
+      }
+    })()
+  }, [])
+  
   return (
     <>
       <span className='self-start bg-darkGrey text-primary text-[28px] px-6 py-2 rounded-full'>
         {type}
       </span>
       <ul className='grid grid-cols-4 gap-8 mb-20'>
-        {showBooks?.map(({ id, title, tag1, tag2, cover }) => (
-          <li key={id} className='cursor-pointer'>
-            <BookCard title={title} tag1={tag1} tag2={tag2} cover={cover} />
+        {showBooks?.map(({ uuid, title_cn, /*tag1, tag2,*/ image }) => (
+          <li key={uuid} className='cursor-pointer'>
+            <BookCard title={title_cn} /*>tag1={tag1} tag2={tag2}*/ cover={image} />
           </li>
         ))}
         <li className='cursor-pointer '>
