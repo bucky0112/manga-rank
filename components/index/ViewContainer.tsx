982eeb999393e74f360fd7347708a0027be32885
bucky0112 <bucky0112@gmail.com>
2022-09-20 01:09:49 +0800

    edit API

--- components/index/ViewContainer.tsx
+++ components/index/ViewContainer.tsx
@@ -3,43 +3,39 @@ import { CardGroup } from './CardGroup'
 import { manga } from '../../lib/api/manga'
 
 interface books {
-  id: number
-  title: string
-  tag1: string
-  tag2: string
-  cover: string
+  uuid: string
+  title_cn: string
+  // tag1: string
+  // tag2: string
+  image: string
 }
 
 const ViewContainer = () => {
-  const [newBooks, setNewBooks] = useState<books[]>([])
-  const [showBooks, setShowBooks] = useState<books[]>([])
+  /*const [showBooks, setShowBooks] = useState<books[]>([])
 
   useEffect(() => {
-    setNewBooks(
-      Array.from({ length: 10 }).map((item, i) => {
-        return {
-          id: i,
-          title: '間諜家家酒',
-          tag1: '喜劇',
-          tag2: '推特話題',
-          cover: 'https://fakeimg.pl/306x430/'
-        }
-      })
-    )
     ;(async () => {
-      const res = await manga.getBooks(10)
-      console.log(res)
+      try {
+        const { data } = await manga.getBooks(10)
+        const res = data?.data?.slice(0, 7)
+        setShowBooks(res)
+      } catch (err) {
+        console.error(err)
+      }
     })()
-  }, [])
+  }, [])*/
 
-  useEffect(() => {
-    setShowBooks(newBooks.slice(0, 7))
-  }, [newBooks])
   return (
     <main className='flex flex-col justify-center gap-10 p-10 bg-index bg-fixed bg-[length:300px] bg-no-repeat'>
-      <CardGroup showBooks={showBooks} type='最新發行' />
-      <CardGroup showBooks={showBooks} type='評價超過8分的作品' />
-      <CardGroup showBooks={showBooks} type='各書店平台週冠軍' />
+      <CardGroup
+        // showBooks={showBooks}
+        type='最新發行' />
+      <CardGroup
+        // showBooks={showBooks}
+        type='評價超過8分的作品' />
+      <CardGroup
+        // showBooks={showBooks}
+        type='各書店平台週冠軍' />
     </main>
   )
 }
--- components/shared/BookCard.tsx
+++ components/shared/BookCard.tsx
@@ -2,13 +2,14 @@ import Image from 'next/image'
 
 interface books {
   title: string,
-  tag1: string,
-  tag2: string,
+  /*tag1: string,
+  tag2: string,*/
   cover: string,
 }
 
 const BookCard = ({ ...rest }:books) => {
-  const { title, tag1, tag2, cover } = rest
+  const { title, /*tag1, tag2,*/ cover } = rest
+  
   return (
     <>
       <div className='grid grid-cols-2 w-[306px] h-[430px] relative'>
@@ -25,14 +26,14 @@ const BookCard = ({ ...rest }:books) => {
         </p>
       </div>
       <p className='text-center text-2xl font-semibold mt-4 mb-4'>{title}</p>
-      <p className='flex justify-center items-center gap-4'>
+      {/* <p className='flex justify-center items-center gap-4'>
         <span className='px-4 py-1 bg-primary rounded-2xl text-xl'>
           {tag1}
         </span>
         <span className='px-4 py-1 bg-primary rounded-2xl text-xl'>
           {tag2}
         </span>
-      </p>
+      </p> */}
     </>
   )
 }
