4d9c43ce660be9b9da8a1532d99aa9bc4f9b6ef5
bucky0112 <bucky0112@gmail.com>
2022-11-04 16:49:51 +0800

    login and logout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
-import { FC, SetStateAction, Dispatch } from 'react'
+import { FC, SetStateAction, Dispatch, useEffect } from 'react'
 import Link from 'next/link'
-// import { useAppSelector, useAppDispatch } from '../store/hooks'
-// import { setUserInfo } from '../store/feat/user/userInfoSlice'
+import { useAppSelector, useAppDispatch } from '../store/hooks'
+import { setUserInfo, selectUserInfo } from '../store/feat/user/userInfoSlice'
 import { useStorage } from '../lib/hooks'
 
 type Props = {
@@ -10,18 +10,28 @@ type Props = {
 }
 
 const Navbar: FC<Props> = ({ setIsOpen, isOpen }) => {
+  const dispatch = useAppDispatch()
   const { storedValue } = useStorage('userInfo', {})
-  console.log(storedValue)
-  
+  const userInfo = useAppSelector(selectUserInfo)
+
+  useEffect(() => {
+    Object.keys(storedValue).length > 0 &&
+      dispatch(setUserInfo({ info: storedValue }))
+  }, [storedValue])
+
   return (
     <nav className='flex items-center justify-between bg-blurGray backdrop-blur-sm px-6 py-2'>
       <h1 className='text-4xl text-mediumGrey font-bold bg-primary rounded-full px-3 py-1'>
         K
       </h1>
       <div className='flex items-center'>
-        <Link href='/user'>
-          <a className='text-darkGrey text-xl mr-10'>登入/註冊</a>
-        </Link>
+        {userInfo?.nickname ? (
+          <p className='text-darkGrey text-xl mr-10'>{userInfo?.nickname}</p>
+        ) : (
+          <Link href='/user'>
+            <a className='text-darkGrey text-xl mr-10'>登入/註冊</a>
+          </Link>
+        )}
         <div
           className={`tham tham-e-squeeze tham-w-6 ${
             isOpen ? 'tham-active' : ''
--- components/index/SideBar.tsx
+++ components/index/SideBar.tsx
@@ -1,23 +1,38 @@
 import { FC } from 'react'
+import { useRouter } from 'next/router'
 import styles from '../../styles/SideBar.module.scss'
+import { useAppSelector } from '../../store/hooks'
+import { selectUserInfo } from '../../store/feat/user/userInfoSlice'
+import { useStorage } from '../../lib/hooks'
 
 type Props = {
   isOpen: boolean
 }
 
-const SideBar: FC<Props> = ({ isOpen }) => (
-  <div className={`${styles.waveBar} ${isOpen && styles.open}`}>
-    <div className={styles.boxCenter}>
-      <ul>
-        <li>會員中心</li>
-        <li>登入/註冊</li>
-        <li>找漫畫</li>
-        <li>近期流行</li>
-        <li>依類別</li>
-        <li>依平台</li>
-      </ul>
+const SideBar: FC<Props> = ({ isOpen }) => {
+  const router = useRouter()
+  const userInfo = useAppSelector(selectUserInfo)
+  const { clearStorage } = useStorage('userInfo', {})
+
+  const handleLogout = () => {
+    clearStorage()
+    router.reload()
+  }
+
+  return (
+    <div className={`${styles.waveBar} ${isOpen && styles.open}`}>
+      <div className={styles.boxCenter}>
+        <ul>
+          <li>會員中心</li>
+          {userInfo?.nickname ? (<li onClick={handleLogout}>登出</li>) : <li>登入/註冊</li>}
+          <li>找漫畫</li>
+          <li>近期流行</li>
+          <li>依類別</li>
+          <li>依平台</li>
+        </ul>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export { SideBar }
