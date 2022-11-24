66db498f67976715df8682b90c85fa7092bd6792
DESKTOP-6EJQH8G\ssd <kiki19990605@gmail.com>
2022-11-24 22:47:23 +0800

    add comment page

---
+++ components/comment/Footer.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from "react"
+// import styles from "../styles/Footer.module.scss"
+
+const Footer: FunctionComponent = () => (
+  <footer className='w-full'>
+    <p className='pt-6 mb-6 border-violet-500 border-t-4 font-semibold'>
+      Copyright © 2022 Yang Zong Lin, Chang Chi Chu, Hsiao-ping Hsu
+    </p>
+  </footer>
+)
+
+export { Footer }
