c485f3e9561e386aefcf6c239864417bf3c44559
bucky0112 <bucky0112@gmail.com>
2022-12-27 21:55:57 +0800

    add score

--- components/index/CardGroup.tsx
+++ components/index/CardGroup.tsx
@@ -9,6 +9,7 @@ interface bookFromApi {
   tag: string
   image: string
   is_adult: number
+  point: string
 }
 
 interface bookTag {
@@ -21,6 +22,7 @@ interface book {
   tag: bookTag[]
   image: string
   is_adult: boolean
+  point: string
 }
 
 interface props {
@@ -61,16 +63,16 @@ const CardGroup = ({ type, apiName }: props) => {
 
   return (
     <>
-      <span className='self-start bg-darkGrey text-primary text-[28px] px-6 py-2 rounded-full'>
+      <span className='self-start bg-darkGrey text-primary text-[28px] px-6 py-2 rounded-full mt-16'>
         {type}
       </span>
       <ul className='grid grid-cols-1 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 gap-x-8 gap-y-14 mb-20'>
-        {showBooks?.map(({ uuid, title_cn, image, tag, is_adult }) => (
+        {showBooks?.map(({ uuid, title_cn, image, tag, is_adult, point }) => (
           <li
             key={uuid}
             className='justify-self-center flex flex-col justify-center items-center w-77 h-135'
           >
-            <BookCard title={title_cn} cover={image} tag={tag} uuid={uuid} isAdult={is_adult} />
+            <BookCard title={title_cn} cover={image} tag={tag} uuid={uuid} isAdult={is_adult} point={point} />
           </li>
         ))}
         <li className='justify-self-center w-77 h-125'>
