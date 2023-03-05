85c6a2cc93d4ede738313b4992b8aab84687a389
bucky0112 <bucky0112@gmail.com>
2023-03-05 18:34:54 +0800

    Update Navbar and ViewContainer to use SideBar state from store

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
-import { FunctionComponent, useState } from 'react'
+import { FunctionComponent } from 'react'
+import { useAppSelector } from 'store/hooks'
+import { selectSideBarOpen } from 'store/feat/share/sideBarSlice'
 import { ViewContainer } from 'components/index/'
 import { Navbar } from 'components/Navbar'
 import { Footer } from 'components/Footer'
 
 const Home: FunctionComponent = () => {
-  const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false)
+  const isOpen = useAppSelector(selectSideBarOpen)
 
   return (
     <>
-      <Navbar setIsOpen={setIsOpenMenu} isOpen={isOpenMenu} />
-      <ViewContainer isOpen={isOpenMenu} />
+      <Navbar isOpen={isOpen} />
+      <ViewContainer isOpen={isOpen} />
       <Footer />
     </>
   )
