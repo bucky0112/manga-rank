46d48ca922fb06fc7456173763fc255c1616406f
bucky0112 <bucky0112@gmail.com>
2023-03-08 00:52:24 +0800

    Update classes in CardGroup and Results components and ViewContainer styles

--- components/index/CardGroup.tsx
+++ components/index/CardGroup.tsx
@@ -52,7 +52,7 @@ const CardGroup = ({ type, apiName }: props) => {
 
   return (
     <>
-      <span className='self-start bg-darkGrey text-primary text-[28px] px-6 py-2 rounded-full mt-16'>
+      <span className='self-start bg-darkGrey text-primary text-[28px] px-6 py-2 rounded-full mt-16 2xl:ml-2'>
         {type}
       </span>
       <ul className='grid grid-cols-1 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 gap-x-8 gap-y-14 mb-20'>
