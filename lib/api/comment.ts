bd5aa851ea79662c48f2a4040bda47ce3dafe1c6
bucky0112 <bucky0112@gmail.com>
2023-03-26 22:43:30 +0800

    Add new comment interface and new comment API endpoint

--- lib/api/comment.ts
+++ lib/api/comment.ts
@@ -3,8 +3,22 @@ import { Endpoints } from "./endpoints";
 
 const client = new APIClient();
 
+interface Comment {
+  chapter: string
+  description: string
+  isThunder: number
+  mangaUuid: string
+  point: number
+}
+
 const comment = {
   getComments: (id: string) => client.get(Endpoints.Comment.Get, `${id}/1`),
-}
+  new: (data: Comment, token: string) => {
+    const headers = {
+      Authorization: token
+    }
+    return client.post(Endpoints.Comment.New, data, headers)
+  },
+};
 
 export { comment }
