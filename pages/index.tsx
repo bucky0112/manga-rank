3484236cc62d993b67b7c9cdd7697b5f093db00b
bucky0112 <bucky0112@gmail.com>
2023-04-05 18:21:03 +0800

    Remove unuse code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
-import { FunctionComponent } from 'react'
 import { useAppSelector } from 'store/hooks'
 import { selectSideBarOpen } from 'store/feat/share/sideBarSlice'
 import { Navbar, Footer } from 'components'
 import { ViewContainer } from 'components/index/'
 
-const Home: FunctionComponent = () => {
+const Home = () => {
   const isOpen = useAppSelector(selectSideBarOpen)
 
   return (
