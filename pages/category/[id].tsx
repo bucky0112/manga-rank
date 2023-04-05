0ed728e6f0eafc55cba148db82a3486ffc1eab4f
bucky0112 <bucky0112@gmail.com>
2023-04-05 18:22:00 +0800

    Category dynamic page done

--- pages/category/[id].tsx
+++ pages/category/[id].tsx
@@ -1,30 +1,34 @@
-import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { useAppSelector } from 'store/hooks'
+import { selectSideBarOpen } from 'store/feat/share/sideBarSlice'
+import { Navbar, Footer, MultipleBooksContainer } from 'components'
+import { useMoreCategory } from 'lib/hooks'
 import { manga } from 'lib/api/manga'
+import categories from 'lib/utils/categories'
 
 const Page = () => {
   const router = useRouter()
   const id = router.query.id as string
 
-  const getCategory = async (id: string, page: string) => {
-    try {
-      const { data } = await manga.getCategory(id, page)
-      console.log(data)
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  const isOpen = useAppSelector(selectSideBarOpen)
 
-  useEffect(() => {
-    if (id) {
-      getCategory(id, '1')
-    }
-  }, [id])
+  const { hasMore, fetchData, showCategory } = useMoreCategory(
+    manga.getCategory,
+    id
+  )
 
   return (
-    <div>
-      <h1>Page</h1>
-    </div>
+    <>
+      <Navbar isOpen={isOpen} />
+      <MultipleBooksContainer
+        type={categories[Number(id) - 1]?.name}
+        isOpen={isOpen}
+        showBooks={showCategory}
+        fetchData={fetchData}
+        hasMore={hasMore}
+      />
+      <Footer />
+    </>
   )
 }
 
