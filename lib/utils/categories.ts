eac155882ef02f47cce74abb8f176d7a164b1248
bucky0112 <bucky0112@gmail.com>
2023-04-05 18:19:20 +0800

    Add categories state

---
+++ lib/utils/categories.ts
@@ -0,0 +1,15 @@
+const categories = [
+  { id: 1, name: '職場社會' },
+  { id: 2, name: '恐怖靈異' },
+  { id: 3, name: '少年熱血' },
+  { id: 4, name: '戀愛故事' },
+  { id: 5, name: '武俠格鬥' },
+  { id: 6, name: '科幻魔幻' },
+  { id: 7, name: '競技體育' },
+  { id: 8, name: '偵探推理' },
+  { id: 9, name: '療癒幽默' },
+  { id: 10, name: 'BL漫畫' },
+  { id: 11, name: '百合漫畫' }
+]
+
+export default categories
