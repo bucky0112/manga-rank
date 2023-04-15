c7dad284fbba87e94fa92a55994017bf67456649
bucky0112 <bucky0112@gmail.com>
2023-04-15 12:54:27 +0800

    r18 cover

--- components/shared/BookCard.tsx
+++ components/shared/BookCard.tsx
@@ -23,19 +23,35 @@ const BookCard = ({ ...rest }: books) => {
   return (
     <>
       <div
-        className='grid grid-cols-2 w-full h-full relative cursor-pointer'
+        className='relative w-full h-full cursor-pointer'
         onClick={() => router.push(`/book/${uuid}`)}
       >
-        <Image
-          className='rounded-3xl'
-          layout='fill'
-          sizes='responsive'
-          src={isAdult ? "/svg/no_adult.svg" : cover}
-          alt={title}
-        />
-        <div className='col-start-2 absolute justify-self-end self-end bg-primary rounded-tl-full rounded-br-[2400px] w-[70%]'>
+        <div className='rounded-3xl relative w-full h-full'>
+          <Image
+            layout='fill'
+            objectFit='cover'
+            src={cover}
+            alt={title}
+            className='rounded-3xl absolute'
+          />
+          {isAdult && (
+            <div className='absolute inset-0 backdrop-blur rounded-3xl flex items-center justify-center'>
+              <Image
+                layout='intrinsic'
+                width={200}
+                height={200}
+                src='/svg/r18Cover.svg'
+                alt='r18 cover'
+                className='rounded-3xl'
+              />
+            </div>
+          )}
+        </div>
+        <div className='absolute right-0 bottom-0 bg-primary rounded-tl-full rounded-br-[2400px] w-[35%]'>
           <p className='flex flex-col items-center justify-start pt-3 ml-3'>
-            <span className='text-5xl font-semibold text-[#565656]'>{point}</span>
+            <span className='text-5xl font-semibold text-[#565656]'>
+              {point}
+            </span>
             <span className='text-darkGrey'>/10人</span>
           </p>
         </div>
