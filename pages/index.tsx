3b9d877d96911cde895ecde16d4aae781c6f47e3
bucky0112 <bucky0112@gmail.com>
2023-04-24 00:02:57 +0800

    Add metadata tags to Head component for social media sharing on the Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { useAppSelector } from 'store/hooks'
 import { selectSideBarOpen } from 'store/feat/share/sideBarSlice'
 import { Navbar, Footer } from 'components'
@@ -8,6 +9,19 @@ const Home = () => {
 
   return (
     <>
+      <Head>
+        <title>Komic</title>
+        <meta property="og:title" content="Komic" />
+        <meta property="og:description" content="最多元的漫畫評論都在漫畫嗑" />
+        <meta property="og:image" content="https://i.imgur.com/TCOUZW1.jpg" />
+        <meta property="og:url" content="https://loquacious-wisp-cab900.netlify.app/" />
+        <meta property="og:type" content="website" />
+        
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Komic" />
+        <meta name="twitter:description" content="最多元的漫畫評論都在漫畫嗑" />
+        <meta name="twitter:image" content="https://loquacious-wisp-cab900.netlify.app/" />
+      </Head>
       <Navbar isOpen={isOpen} />
       <ViewContainer isOpen={isOpen} />
       <Footer />
