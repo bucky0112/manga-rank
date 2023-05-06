b000de89eaeb468cf3b27931afec32033b34584e
sabala36mia <kiki19990605@gmail.com>
2023-05-07 00:45:02 +0800

    首頁書本間距，Navbar RWD 調整完成

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,6 +8,7 @@ import { setSideBarOpen } from 'store/feat/share/sideBarSlice'
 import { useStorage } from 'lib/hooks'
 import { IoSearchOutline } from 'react-icons/io5'
 import { search } from 'lib/api/search'
+import { RiLoginBoxLine } from 'react-icons/ri'
 
 type Props = {
   isOpen: boolean
@@ -38,20 +39,36 @@ const Navbar: FC<Props> = ({ isOpen }) => {
     }
   }
 
+  // 當width小於600px顯示icon
+  const [showIcon, setSshowIcon] = useState(false)
+
+  useEffect(() => {
+    function handleResize() {
+      setSshowIcon(window.innerWidth < 600)
+    }
+
+    window.addEventListener('resize', handleResize)
+    handleResize()
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
     <nav
       onMouseEnter={() => setIsShowSearch(true)}
       onMouseLeave={() => setIsShowSearch(false)}
-      className='grid grid-cols-12 items-center bg-blurGray backdrop-blur-sm px-6 py-4 font-inter fixed z-50 w-full'
+      className='flex 2xl:gap-[117px] xl:gap-[53px] slg:gap-[40px] xls:gap-[26px] xs:gap-[27px] items-center bg-blurGray backdrop-blur-sm px-6 py-4 font-inter fixed z-50 w-full'
     >
       <Link href='/'>
-        <a className='col-span-1 place-self-center'>
+        <a className='flex-[0.1] pl-10 place-self-center'>
           <h1 className='flex justify-center items-center text-xl text-mediumGrey font-bold bg-primary rounded-full w-7 h-7'>
             K
           </h1>
         </a>
       </Link>
-      <div className='col-start-3 col-end-10'>
+      <div className='flex-[4]'>
         {isShowSearch ? (
           <input
             type='text'
@@ -63,17 +80,24 @@ const Navbar: FC<Props> = ({ isOpen }) => {
           <IoSearchOutline className='text-2xl text-darkGrey' />
         )}
       </div>
-      <div className='col-start-10 col-span-3 flex items-center justify-around'>
+      <div className='flex-1 2xl:gap-[117px] xl:gap-[53px] slg:gap-[40px] xls:gap-[26px] xs:gap-[27px] flex items-center justify-around'>
         {userInfo?.nickname ? (
           <p className='text-darkGrey text-xl mr-10'>{userInfo?.nickname}</p>
         ) : (
-          <Link href='/user'>
-            <a className='text-darkGrey text-xl mr-10'>登入/註冊</a>
-          </Link>
+          <>
+            <Link href='/user'>
+              <a className='text-darkGrey text-xl whitespace-nowrap'
+                style={{ display: showIcon ? 'none' : 'inline-block' }}>登入/註冊</a>
+            </Link>
+            <Link href='/user'>
+              <a className='text-darkGrey text-2xl whitespace-nowrap'
+                style={{ display: showIcon ? 'inline-block' : 'none' }}><RiLoginBoxLine /></a>
+            </Link>
+          </>
         )}
         <div
           className={classNames({
-            'tham tham-e-squeeze tham-w-6': true,
+            'tham tham-e-squeeze tham-w-6 mr-[43px]': true,
             'tham-active': isOpen
           })}
           onClick={() => dispatch(setSideBarOpen({ isOpen: !isOpen }))}
