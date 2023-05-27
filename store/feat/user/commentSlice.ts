1fa1a87d2ff5ac9da4d7b86d7d441a24aa8a8e59
bucky0112 <bucky0112@gmail.com>
2023-05-27 23:05:33 +0800

    Add CommentDetail interface and setCommentDetail reducer to commentSlice

--- store/feat/user/commentSlice.ts
+++ store/feat/user/commentSlice.ts
@@ -2,24 +2,46 @@ import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import type { RootState } from '../..'
 
+interface CommentDetail {
+  description?: string
+  isThunder?: number
+  point?: string
+  chapter?: string
+  bookTitle?: string
+}
+
 interface CommentState {
   editPermission: boolean
+  commentDetail: CommentDetail
 }
 
 const initialState: CommentState = {
-  editPermission: false
+  editPermission: false,
+  commentDetail: {
+    description: '',
+    isThunder: 0,
+    point: '0',
+    chapter: '',
+    bookTitle: ''
+  }
 }
 
 export const commentSlice = createSlice({
   name: 'comment',
   initialState,
   reducers: {
-    setEditPermission: (state, action: PayloadAction<CommentState>) => {
-      state.editPermission = action.payload.editPermission
+    setEditPermission: (state, action: PayloadAction<boolean>) => {
+      state.editPermission = action.payload
+    },
+    setCommentDetail: (state, action: PayloadAction<CommentDetail>) => {
+      state.commentDetail = action.payload
     }
   }
 })
 
-export const { setEditPermission } = commentSlice.actions
-export const selectEditPermission = (state: RootState) => state.comment.editPermission
+export const { setEditPermission, setCommentDetail } = commentSlice.actions
+export const selectEditPermission = (state: RootState) =>
+  state.comment.editPermission
+export const selectCommentDetail = (state: RootState) =>
+  state.comment.commentDetail
 export default commentSlice.reducer
