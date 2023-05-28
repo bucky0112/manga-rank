1f8f7076aa25b4682a67626159356fd41e46d28d
bucky0112 <bucky0112@gmail.com>
2023-05-28 15:55:32 +0800

    Add deletePermission property to CommentState and setDeletePermission to update it

--- store/feat/user/commentSlice.ts
+++ store/feat/user/commentSlice.ts
@@ -9,22 +9,26 @@ interface CommentDetail {
   chapter?: string
   bookTitle?: string
   mangaUuid?: string
+  uuid?: string
 }
 
 interface CommentState {
   editPermission: boolean
+  deletePermission: boolean
   commentDetail: CommentDetail
 }
 
 const initialState: CommentState = {
   editPermission: false,
+  deletePermission: false,
   commentDetail: {
     description: '',
     isThunder: 0,
     point: '0',
     chapter: '',
     bookTitle: '',
-    mangaUuid: ''
+    mangaUuid: '',
+    uuid: ''
   }
 }
 
@@ -35,15 +39,20 @@ export const commentSlice = createSlice({
     setEditPermission: (state, action: PayloadAction<boolean>) => {
       state.editPermission = action.payload
     },
+    setDeletePermission: (state, action: PayloadAction<boolean>) => {
+      state.deletePermission = action.payload
+    },
     setCommentDetail: (state, action: PayloadAction<CommentDetail>) => {
       state.commentDetail = action.payload
-    }
+    },
   }
 })
 
-export const { setEditPermission, setCommentDetail } = commentSlice.actions
+export const { setEditPermission, setCommentDetail, setDeletePermission } = commentSlice.actions
 export const selectEditPermission = (state: RootState) =>
   state.comment.editPermission
 export const selectCommentDetail = (state: RootState) =>
   state.comment.commentDetail
+export const selectDeletePermission = (state: RootState) =>
+  state.comment.deletePermission
 export default commentSlice.reducer
