5aa0fbb42a224f42b249da3e4238f8baab8f70f8
bucky0112 <bucky0112@gmail.com>
2023-06-18 20:29:18 +0800

    Add a fixed position to the footer element at the bottom of the page for better UI/UX

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from "react"
 import styles from "../styles/Footer.module.scss"
 
 const Footer: FunctionComponent = () => (
-  <footer className='w-full font-inter'>
+  <footer className='fixed bottom-0 w-full font-inter'>
     <div className={styles.footer}>
       <p>KOMIC</p>
     </div>
